7f0df810f8d9714649f8de09d6d61f8a3d59c4ab
emirosely <124699888+emirosely@users.noreply.github.com>
2023-05-24 17:45:56 +0100

    Content updating, css styling & organisation, update to button function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function learnMore() {
-  let name = prompt("What is your name?");
   let computer = prompt("Are you a human or AI?");
   computer = computer.toLowerCase();
   computer = computer.trim();
 
   if (computer === "human") {
-    alert("Hi, " + name + "! You may learn more about this study.");
+    window.location =
+      "https://sensilab.monash.edu/phd-projects/the-ethics-of-ai-art/";
   } else {
-    alert("Nice try, " + name + "... but you're the subject of this study.");
+    alert("Nice try... but you're the subject of this study.");
   }
 }
 let applyButton = document.querySelector("button");
